172a97cb94b4d3faed05b25532a1d55c96c8c1d8
hikskks <111577466+hikskks@users.noreply.github.com>
2022-11-19 16:42:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+function pergunta(pergunta){
+    var nome = (prompt (pergunta));
+
+    var elementoNome = document.getElementById("nome")
+    elementoNome.innerText = "olá " + nome
+}
+
+function mudarCor(cor){
+    var box = document.getElementById("box")
+    var nome = document.getElementById("nome")
+    var texto = document.getElementById("texto")
+
+    box.style.backgroundColor = cor
+    nome.style.color = cor
+    texto.style.color = cor
+}
+
+function mudarTextoPassa(){
+    var passa = 'clica no bloco'
+    document.getElementById("texto").innerHTML = passa
+}
+
+function mudarTextoFora(){
+    var fora = 'passe o mouse em cima do bloco'
+    document.getElementById("texto").innerHTML = fora
+}
+
+function mudarTextoClique(){
+    var clique = 'clique duas vezes'
+    document.getElementById("texto").innerHTML = clique
+}
+
+function mudarTextoDoisCliques(){
+    var doiscliques = 'digite seu nome'
+    document.getElementById("texto").innerHTML = doiscliques
+}
